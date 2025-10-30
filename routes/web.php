a8b548afd3ab38c00270a4a793a43182b895767c
Franzz-jeremy <nasutionabdul247@gmail.com>
2025-10-30 18:28:36 +0700

    penambahan tabel produk menggunakan array

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('index');
 });
+
+Route::get('/produk', function () {
+    $kode = ['BRG001', 'BRG002'];
+    $nama = ['pena', 'buku'];
+    $jenis = ['alat tulis', 'elektronik', 'sembako'];
+    $harga = ['15000', '20000'];
+    $jumlah = count($kode);
+    return view('produk', compact('kode', 'nama', 'jenis', 'harga', 'jumlah'));
+});
